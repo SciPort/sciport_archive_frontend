b83d11ec5868bfdfd84e24f45dbf5e926d564ec4
DoHi0512 <donghuni512@gmail.com>
2022-10-11 11:45:47 +0900

    UPDATE::(#5) update route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Login from "../src/components/page/login"
+import Login from "../src/components/page/login";
 import Header from "./components/Header";
+import Main from "./pages/main";
 function App() {
   return (
     <>
       <Header />
       <Routes>
         <Route exact path="/login" element={<Login />} />
+        <Route exact path="/" element={<Main />} />
       </Routes>
     </>
   );
