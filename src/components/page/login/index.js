65d92fffd7bccb9c09e758d61c6862f4a27a8f83
LLTae2 <taehyun5820@gmail.com>
2022-10-11 11:39:38 +0900

    Update::(#4) Update login page

--- src/components/page/login/index.js
+++ src/components/page/login/index.js
@@ -4,7 +4,17 @@ import * as S from "./style";
 const index = () => {
   return (
     <S.Wrapper>
-      <S.Login></S.Login>
+      <S.Login>
+        <span>로그인</span>
+        <p>회원 서비스를 이용하려면 로그인이 필요합니다.<br/>아이디와 비밀번호를 입력 후 로그인 하십시오.</p>
+        <S.LoginForm>
+          <div>
+            <input placeholder="아이디 입력" type='text' />
+            <input placeholder="비밀번호 입력" type='password' />
+          </div>
+          <input placeholder="로그인" type='submit' />
+        </S.LoginForm>
+      </S.Login>
     </S.Wrapper>
   );
 };
