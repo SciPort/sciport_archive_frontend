8fb6919f953d991f7a358bf75bf2903f62468895
LLTae2 <taehyun5820@gmail.com>
2022-10-25 10:21:55 +0900

    Update::회원가입 페이지 라우팅

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login/index";
 import Header from "./components/common/header";
 import Main from "./pages/main"
+import Register from "./pages/register/index";
 function App() {
   return (
     <>
       <Header />
       <Routes>
         <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
         <Route exact path="/" element={<Main />} />
       </Routes>
     </>
