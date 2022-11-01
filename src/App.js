00a7d0edb7afbc02b9a52da1c399083900438c01
LLTae2 <taehyun5820@gmail.com>
2022-11-01 10:26:28 +0900

    Update::app.js 라우팅 수정

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login/index";
 import Header from "./components/common/header";
-import Main from "./pages/main"
+import Main from "./pages/main";
 import Register from "./pages/register/index";
+import Detail from "./pages/detail/index";
 function App() {
   return (
     <>
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/" element={<Main />} />
+        <Route exact path="/detail" element={<Detail />} />
       </Routes>
     </>
   );
