6cbcac78d3f0525f09d644a26442d3c843377a11
LLTae2 <taehyun5820@gmail.com>
2022-11-01 12:09:49 +0900

    Update::회원가입에 이름 추가

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -3,6 +3,7 @@ import * as S from "./style";
 
 const Index = () => {
   const [registerData, setRegisterData] = useState({
+    name: "",
     id: "",
     password: "",
     passwordCheck: "",
@@ -11,33 +12,45 @@ const Index = () => {
     <S.Wrapper>
       <S.Register>
         <span>Register</span>
-        <p>회원가입을 완료하려면 모든 입력창에<br/>정보를 입력한 뒤 등록버튼을 눌러주세요.</p>
-        <S.RegisterForm 
+        <p>
+          회원가입을 완료하려면 모든 입력창에
+          <br />
+          정보를 입력한 뒤 등록버튼을 눌러주세요.
+        </p>
+        <S.RegisterForm
         // onSubmit={LoginFunc}
         >
-          <input 
+          <input
             // onChange={(e) => {
             //   setLoginData({...loginData, id: e.target.value})
             //   console.log(loginData.id)
             // }}
-            placeholder="아이디" 
-            type='text' 
+            placeholder="이름"
+            type="text"
           />
-          <input 
+          <input
+            // onChange={(e) => {
+            //   setLoginData({...loginData, id: e.target.value})
+            //   console.log(loginData.id)
+            // }}
+            placeholder="아이디"
+            type="text"
+          />
+          <input
             // onChange={(e) => {
             //   setLoginData({...loginData, password: e.target.value})
             //   console.log(loginData.password)
             // }}
-            placeholder="비밀번호" 
-            type='password' 
+            placeholder="비밀번호"
+            type="password"
           />
-          <input 
+          <input
             // onChange={(e) => {
             //   setLoginData({...loginData, password: e.target.value})
             //   console.log(loginData.password)
             // }}
-            placeholder="비밀번호 확인" 
-            type='password' 
+            placeholder="비밀번호 확인"
+            type="password"
           />
           <input value="회원가입" type="submit" />
         </S.RegisterForm>
@@ -45,7 +58,7 @@ const Index = () => {
           {/* <button style={{backgroundColor: "#2D97F8"}}>회원가입</button>
           <button style={{backgroundColor: "#2D72F8"}}>아이디/비밀번호 찾기</button> */}
         </S.SubmitForm>
-     </S.Register>
+      </S.Register>
     </S.Wrapper>
   );
 };
