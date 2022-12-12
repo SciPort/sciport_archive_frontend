145b5b3bc5621e50dd270b629e74d6dcaae45ff9
DoHi0512 <donghuni512@gmail.com>
2022-12-12 17:35:56 +0900

    refactoring::main

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { atom, useRecoilState, useRecoilValue } from "recoil";
 import Lecture from "../../components/lecture";
 import { userState } from "../../components/states";
 import { list } from "../../assets/data/export";
@@ -12,83 +11,86 @@ import {
   AiOutlineCheck,
 } from "react-icons/ai";
 import { BsArrowUpShort, BsArrowDownShort } from "react-icons/bs";
-import { Category1, Category2, Category3, SetState } from "../../App";
 export default function Main() {
-  const [check, setCheck] = useRecoilState(SetState);
+  const [check, setCheck] = useState(new Set());
   const [bool, setBool] = useState(true);
-  const [cate1, setCate1] = useRecoilState(Category1);
-  const [cate2, setCate2] = useRecoilState(Category2);
-  const [cate3, setCate3] = useRecoilState(Category3);
+  const [cate, setCate] = useState([[], [], []]);
   const [inp, setInp] = useState("");
-
+  const [chg, setChg] = useState(false);
   const title = ["교육", "학기", "교실"];
-  function sub() {
-    const form = {
-      educations: cate1,
-      terms: cate2,
-      lessons: cate3,
-      name: inp,
-      page: 1,
-    };
-    console.log(form);
-    axios
-      .post("http://192.168.10.128:8080/lecture/getByCate", form)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+  const list = [
+    ["개인교육", "단체교육", "성인교육"],
+    [
+      "겨울학기",
+      "봄학기(1)",
+      "봄학기(2)",
+      "여름학기",
+      "가을학기(1)",
+      "가을학기(2)",
+    ],
+    [
+      "유아과학교실",
+      "창의탐구교실",
+      "실험탐구교실",
+      "SW코딩교실",
+      "창작메이커교실",
+      "프로젝트교실",
+      "후원회교육",
+    ],
+  ];
+  // function sub() {
+  //   const form = {
+  //     educations: cate1,
+  //     terms: cate2,
+  //     lessons: cate3,
+  //     name: inp,
+  //     page: 1,
+  //   };
+  //   console.log(form);
+  //   axios
+  //     .post("http://192.168.10.128:8080/lecture/getByCate", form)
+  //     .then((res) => {
+  //       console.log(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }
   const Drops = list.map((data1, idx1) => (
     <S.CateWrapper>
       <S.Cate>
         <span>{title[idx1]}</span>
         <div>
-          {" "}
           <BsArrowDownShort className="icon" size={"70%"} />
         </div>
       </S.Cate>
       <div className="dropdown">
-        {data1.map((data, idx2) => (
-          <S.DropItem
-            color={
-              check.has(idx1.toString() + idx2.toString()) ? "white" : "#000000"
-            }
-            bgcolor={
-              check.has(idx1.toString() + idx2.toString())
-                ? "#0c2136"
-                : "#ffffff"
-            }
-            onClick={() => {
-              check.has(idx1.toString() + idx2.toString())
-                ? idx1 === 0
-                  ? setCate1(cate1?.filter((data) => data != list[idx1][idx2]))
-                  : idx1 === 1
-                  ? setCate2(cate2?.filter((data) => data != list[idx1][idx2]))
-                  : setCate3(cate3?.filter((data) => data != list[idx1][idx2]))
-                : idx1 === 0
-                ? setCate1(cate1?.concat(list[idx1][idx2]))
-                : idx1 === 1
-                ? setCate2(cate2?.concat(list[idx1][idx2]))
-                : setCate3(cate3?.concat(list[idx1][idx2]));
-
-              check.has(idx1.toString() + idx2.toString())
-                ? check.delete(idx1.toString() + idx2.toString())
-                : check.add(idx1.toString() + idx2.toString());
-              setCheck(check);
-            }}
-          >
-            <span>{data}</span>
-            {check.has(idx1.toString() + idx2.toString()) ? (
-              <AiOutlineCheck className="icon" />
-            ) : null}
-          </S.DropItem>
-        ))}
+        {data1.map((data, idx2) => {
+          let code = idx1.toString() + idx2.toString();
+          return (
+            <S.DropItem
+              color={check.has(code) ? "white" : "#000000"}
+              bgcolor={check.has(code) ? "#0c2136" : "#ffffff"}
+              onClick={() => {
+                check.has(code)
+                  ? (cate[idx1] = cate[idx1]?.filter(
+                      (data) => data != list[idx1][idx2]
+                    ))
+                  : (cate[idx1] = cate[idx1]?.concat(list[idx1][idx2]));
+                setCate(cate);
+                check.has(code) ? check.delete(code) : check.add(code);
+                setCheck(check);
+                setChg(!chg);
+              }}
+            >
+              <span>{data}</span>
+              {check.has(code) ? <AiOutlineCheck className="icon" /> : null}
+            </S.DropItem>
+          );
+        })}
       </div>
     </S.CateWrapper>
   ));
-  console.log(check);
   return (
     <S.Layout>
       <S.Img src="https://www.sciport.or.kr/homepage/kor/_Img/Layout/svisual_MN035.jpg" />
